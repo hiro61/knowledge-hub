5669350c1f30f2a4fabce940690450bf74a654ba
hiroto <ryu.18612.ruby.5101126@gmail.com>
2026-05-09 15:24:10 +0900

    Use direct article navigation

--- script.js
+++ script.js
@@ -70,7 +70,6 @@ const GENRE_ICONS = {
 let allArticles = [];
 let currentTab = "new";
 let currentSearchQuery = "";
-let activeArticleFile = "";
 
 const contentArea = document.getElementById("contentArea");
 const tabButtons = Array.from(document.querySelectorAll(".tab-button"));
@@ -111,9 +110,7 @@ function initAuthGate() {
 }
 
 function initViewer() {
-  document.getElementById("closeViewerButton").addEventListener("click", handleViewerBack);
-  window.addEventListener("popstate", syncViewerWithHistory);
-  syncViewerWithHistory();
+  closeArticle();
 }
 
 function initTabs() {
@@ -464,47 +461,13 @@ function openArticle(article) {
   if (!article.file) {
     return;
   }
-  activeArticleFile = article.file;
-  articleFrame.src = article.file;
-  articleViewer.classList.remove("hidden");
-  articleViewer.setAttribute("aria-hidden", "false");
-  const nextState = { view: "article", article: article.file };
-  if (!isArticleState(window.history.state, article.file)) {
-    window.history.pushState(nextState, "", window.location.href);
-  }
+  window.location.assign(article.file);
 }
 
 function closeArticle() {
-  activeArticleFile = "";
   articleViewer.classList.add("hidden");
   articleViewer.setAttribute("aria-hidden", "true");
   articleFrame.src = "";
-}
-
-function handleViewerBack() {
-  if (isArticleState(window.history.state)) {
-    window.history.back();
-    return;
-  }
-  closeArticle();
-}
-
-function syncViewerWithHistory() {
-  if (isArticleState(window.history.state)) {
-    const nextFile = window.history.state.article;
-    if (nextFile && activeArticleFile !== nextFile) {
-      activeArticleFile = nextFile;
-      articleFrame.src = nextFile;
-    }
-    articleViewer.classList.remove("hidden");
-    articleViewer.setAttribute("aria-hidden", "false");
-    return;
-  }
-  closeArticle();
-}
-
-function isArticleState(state, file = activeArticleFile) {
-  return Boolean(state && state.view === "article" && state.article === file);
 }
 
 function getArticleId(article) {
